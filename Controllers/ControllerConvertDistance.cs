c3c7f4714156cdb8737b3681bddcbd52953468bd
NikitaSergeevichh <120660132+NikitaSergeevichh@users.noreply.github.com>
2023-01-15 17:11:21 +0300

    1

---
+++ Controllers/ControllerConvertDistance.cs
@@ -0,0 +1,19 @@
+﻿using JSONApiApp.Service;
+
+namespace JSONApiApp.Controlles
+{
+    public class ControllerConvertDistance
+    {
+        private ServiceConvertDistance convertDistance;
+
+        public ControllerConvertDistance(ServiceConvertDistance convertDistance)
+        {
+            this.convertDistance = convertDistance;
+        }        
+        public async Task ConvertNumber(HttpContext context)
+        {          
+            
+
+        }
+    }
+}
---
+++ Controllers/MainController.cs
@@ -0,0 +1,35 @@
+﻿using JSONApiApp.Message;
+using JSONApiApp.Model.Entity;
+using JSONApiApp.Service;
+using static JSONApiApp.Message.KnowHostMessage;
+using static JSONApiApp.Message.RequestMessage;
+
+namespace JSONApiApp.Controlles
+{
+    public class MainController
+    {
+        ServiceKnowHost serviceKnowHost;
+        ServiceMyRequest serviceMyRequest;
+        public async Task Status(HttpContext context)
+        {
+            await context.Response.WriteAsJsonAsync(new BaseMessage.StatusMessage
+                (context.Request.Host.Port, System.Net.Dns.GetHostName()));
+        }
+
+        public async Task Info(HttpContext context)
+        {
+            await context.Response.WriteAsJsonAsync(new BaseMessage.StringMessage("/ping, /status, /info"));
+        }
+        public async Task KnownHost(HttpContext context)
+        {
+            List<KnowHost> knowHostsList = serviceKnowHost.GetKnowHost();
+            await context.Response.WriteAsJsonAsync(new KnowHostList(knowHostsList));
+        }
+        public async Task HostRequest(HttpContext context)
+        {
+            List<MyRequest> requestList = serviceMyRequest.GetMyRequest();
+            await context.Response.WriteAsJsonAsync(new RequestList(requestList));
+        }
+
+    }
+}
